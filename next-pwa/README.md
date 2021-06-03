3d022ea59d91c4698ae04277feec0f2ac70c0506
Nenad <nenad.p.pejic@gmail.com>
2021-06-03 19:45:29 +0200

    Completed next-pwa

--- next-pwa/README.md
+++ next-pwa/README.md
@@ -4,8 +4,53 @@ Nexp app into Progressive Web App.
 
 ## Installation
 
-Created with
+Create with
 
 ```bash
 $ npx create-next-app
+```
+
+Add next-pwa
+
+```bash
+$ npm install next-pwa
+```
+
+Create /next.config.js
+
+```js
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
+});
+```
+
+Create manifest, favicon and images in /public
+
+```bash
+# manifest.webmanifest with 5 .png images
+https://www.simicart.com/manifest-generator.html/
+
+# favicon.ico
+https://www.favicon-generator.org/
+
+# apple-touch-icon.png wich is 180x180
+```
+
+Test
+
+```bash
+$ npm run build
+$ npm run start
+```
+
+Optional: Add auto-generated service worker to .gitignore
+
+```bash
+# next-pwa
+/public/sw.js
+/public/workbox-*.js
 ```
---
+++ next-pwa/components/HeadComponent.js
@@ -0,0 +1,32 @@
+import Head from "next/head"
+
+const HeadComponent = ({ title, description, url }) => {
+  return (
+    <Head>
+      <meta charSet='utf-8' />
+      <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='description' content={description} />
+      <meta name='keywords' content='example keywords' />
+      <meta name="theme-color" content="#ffffff"></meta>
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <link rel="canonical" href="https://www.example.com" />
+      <link rel="alternate" hrefLang="en" href="https://www.example.com/en" />
+      <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+      <title>{title}</title>
+
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content="Example Name" />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content="http://www.example.com/og-image.jpg" />
+      <meta property="og:image:secure_url" content="https://www.example.com/og-image.jpg" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="fb:app_id" content="1234567890" />
+    </Head>
+  )
+}
+
+export default HeadComponent
