08b2124d00e673e6d1d4ba089fbc5078a9777480
Nenad <nenad.p.pejic@gmail.com>
2021-06-22 11:58:30 +0200

    Modified BasicTable

--- react-material-ui-ts/src/components/tables/BasicTable/index.tsx
+++ react-material-ui-ts/src/components/tables/BasicTable/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import React from 'react'
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
-});
+})
 
 function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
+  return { name, calories, fat, carbs, protein }
 }
 
 const rows = [
@@ -24,10 +24,10 @@ const rows = [
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
   createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+]
 
-export default function BasicTable() {
-  const classes = useStyles();
+const BasicTable = () => {
+  const classes = useStyles()
 
   return (
     <TableContainer component={Paper}>
@@ -36,9 +36,9 @@ export default function BasicTable() {
           <TableRow>
             <TableCell>Dessert (100g serving)</TableCell>
             <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="right">Fat&nbsp(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp(g)</TableCell>
+            <TableCell align="right">Protein&nbsp(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -54,5 +54,7 @@ export default function BasicTable() {
         </TableBody>
       </Table>
     </TableContainer>
-  );
+  )
 }
+
+export default BasicTable
