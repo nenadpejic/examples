a11afa93788173054366a3547723409a2b368b19
Nenad <nenad.p.pejic@gmail.com>
2021-07-29 13:29:57 +0200

    Added getAllTodosUsingFetch thunk action

--- react-redux-ts/src/redux/todos/todosActions.ts
+++ react-redux-ts/src/redux/todos/todosActions.ts
@@ -31,3 +31,29 @@ export const getAllTodos = (): ThunkAction<
       dispatch({ type: "todosGetAll/rejected", error: { message } })
     }
   }
+
+// Using fetch
+export const getAllTodosUsingFetch = (): ThunkAction<
+  void, // TReturnType
+  RootState, // TState
+  unknown, // TExtraThunkARg
+  TodosGetAllAction // TBasicAction
+> => async (
+  dispatch,
+  getState
+) => {
+    dispatch({ type: "todosGetAll/pending" })
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+
+    if (response.ok) {
+
+      const data = await response.json()
+      dispatch({ type: "todosGetAll/fulfilled", payload: data })
+
+    } else {
+
+      const message = "Failed to fetch data!"
+      dispatch({ type: "todosGetAll/rejected", error: { message } })
+    }
+  }
