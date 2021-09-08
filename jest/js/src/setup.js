27bb57bbc797e8f8fabc1002547605f24eac0829
Nenad <nenad.p.pejic@gmail.com>
2021-09-08 14:41:07 +0200

    Removed unused examples

--- jest/js/src/setup.js
+++
@@ -1,6 +0,0 @@
-const functions = {
-  initializeDatabase: () => { console.log('Database initialized!') },
-  clearDatabase: () => { console.log('Database cleared!') }
-}
-
-module.exports = functions
--- react-jest-ts/src/components/examples/Matchers.test.tsx
+++
@@ -1,6 +0,0 @@
-import { screen } from '@testing-library/react'
-
-test("if input elements rendered correctly", () => {
-  const passwordEl_v1 = screen.getByLabelText("Password *")
-  const passwordEl_v2 = screen.getByPlaceholderText(/password/i)
-})
