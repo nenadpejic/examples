5cff6090e6425acc593be930ab3fb821553bca51
Nenad <nenad.p.pejic@gmail.com>
2021-10-01 18:52:11 +0200

    Created react-eslint-ts

---
+++ react-eslint-ts/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+function App() {
+  const x = 1;
+
+  useEffect(() => {
+    console.log(x);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
